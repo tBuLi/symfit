a3412921921159fc77a9821c9963e70b842023eb
tBuLi <martin.roelfs@telfort.nl>
2014-11-21 11:36:17 +0100

    setup.py added.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='symfit',
+    version='0.1',
+    description='Symbolic Fitting; fitting as it should be.',
+    author='Martin Roelfs',
+    author_email='m.roelfs@rug.nl',
+    url='symfit.readthedocs.org',
+    packages=['symfit', 'symfit.core'],
+)
