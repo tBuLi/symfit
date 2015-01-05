65b0ca6e0da9a0450191fecb957d027cc9c10371
tBuLi <martin.roelfs@telfort.nl>
2015-01-05 11:36:32 +0100

    Upgraded to the next arbitrary function.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='symfit',
-    version='0.2.1',
+    version='0.2.2',
     description='Symbolic Fitting; fitting as it should be.',
     author='Martin Roelfs',
     author_email='m.roelfs@rug.nl',
