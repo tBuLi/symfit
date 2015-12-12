1d5e92eae6209ca1dad216b6f3dfaf301b4fd8ad
Martin Roelfs <tbuli@rn-145-97-173-161.eduroam.rug.nl>
2015-12-12 14:28:23 +0100

    Made inspect py2 compatible.

--- symfit/core/operators.py
+++ symfit/core/operators.py
@@ -1,18 +1,21 @@
 """
 Monkey Patching module.
 
-
 This module makes ``sympy`` Expressions callable, which makes the whole project feel more consistent.
 """
-import inspect
+import sys
 
 from sympy import Eq, Ne
-# from sympy.core import expr
 from sympy.core.expr import Expr
 import sympy
 import warnings
 from symfit.core.support import sympy_to_py, seperate_symbols
 from symfit.core.argument import Parameter
+
+if sys.version_info >= (3,0):
+    import inspect as inspect_sig
+else:
+    import funcsigs as inspect_sig
 
 # # Overwrite the behavior opun equality checking. But we want to be able to fall
 # # back on default behavior.
@@ -58,23 +61,23 @@ def call(self, *values, **named_values):
     on the Expr before calling it anyway, which makes calculating the signature absolutely useless.
     However, I hope that someday some monkey patching expert in shining armour comes by and finds
     a way to store it in __signature__ upon __init__ of any ``symfit`` expr such that calling
-    inspect.signature on a symbolic expression will tell you which arguments to provide.
+    inspect_sig.signature on a symbolic expression will tell you which arguments to provide.
 
     :param self: Any subclass of sympy.Expr
     :param values: Values for the Parameters and Variables of the Expr.
     :param named_values: Values for the vars and params by name. ``named_values`` is
         allowed to contain too many values, as this sometimes happens when using
         **fit_result.params on a submodel. The irrelevant params are simply ignored.
-    :return: The function evaluated at ``values``. Depending on the Expr and ``values``,
-        this could be a single number or an array.
+    :return: The function evaluated at ``values``. The type depends entirely on the input.
+        Typically an array or a float but nothing is enforced.
     """
     independent_vars, params = seperate_symbols(self)
     # Convert to a pythonic function
     func = sympy_to_py(self, independent_vars, params)
 
     # Handle args and kwargs according to the allowed names.
-    parameters = [  # Note that these are inspect.Parameter's, not symfit parameters!
-        inspect.Parameter(arg.name, inspect.Parameter.POSITIONAL_OR_KEYWORD)
+    parameters = [  # Note that these are inspect_sig.Parameter's, not symfit parameters!
+        inspect_sig.Parameter(arg.name, inspect_sig.Parameter.POSITIONAL_OR_KEYWORD)
             for arg in independent_vars + params
     ]
 
@@ -83,7 +86,7 @@ def call(self, *values, **named_values):
         name: value for name, value in named_values.items() if name in arg_names
     }
 
-    signature = inspect.Signature(parameters=parameters)
+    signature = inspect_sig.Signature(parameters=parameters)
     bound_arguments = signature.bind(*values, **relevant_named_values)
 
     return func(**bound_arguments.arguments)
