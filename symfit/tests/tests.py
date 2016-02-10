23af08be7699e91b70770d36d21fa41f5494c502
Peter <Pitje06@users.noreply.github.com>
2016-02-10 11:59:47 +0100

    Resolved issue #50
    Resolved by adding a setup method that initalizes the random seed, and gives an initial guess for A.
    
    Further, did some pep8 cleaning, but it's still a mess.

--- symfit/tests/tests.py
+++ symfit/tests/tests.py
@@ -7,12 +7,18 @@ import numpy as np
 from symfit.api import Variable, Parameter, Fit, FitResults, Maximize, Minimize, exp, Likelihood, ln, log, variables, parameters
 from symfit.functions import Gaussian, Exp
 import scipy.stats
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, minimize
 from symfit.core.support import sympy_to_scipy, sympy_to_py
-import matplotlib.pyplot as plt
-import seaborn
+#import matplotlib.pyplot as plt
+#import seaborn
+
 
 class TddInPythonExample(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Make sure test results are consistent across iterations
+        np.random.seed(0)
+
     def test_gaussian(self):
         x0, sig = parameters('x0, sig')
         x = Variable()
@@ -29,8 +35,8 @@ class TddInPythonExample(unittest.TestCase):
         result = func(x=2, y=3, a=3, b=9)
         self.assertEqual(result, 3*2**2 + 9*3**2)
 
-        xdata = np.arange(1,10)
-        ydata = np.arange(1,10)
+        xdata = np.arange(1, 10)
+        ydata = np.arange(1, 10)
         result = func(x=ydata, y=ydata, a=3, b=9)
         self.assertTrue(np.array_equal(result, 3*xdata**2 + 9*ydata**2))
 
@@ -38,7 +44,7 @@ class TddInPythonExample(unittest.TestCase):
         """
         Fit results should be read-only. Let's try to break this!
         """
-        xdata = np.linspace(1,10,10)
+        xdata = np.linspace(1, 10, 10)
         ydata = 3*xdata**2
 
         a = Parameter(3.0, min=2.75)
@@ -53,24 +59,27 @@ class TddInPythonExample(unittest.TestCase):
         try:
             fit_result.params = 'hello'
         except AttributeError:
-            self.assertTrue(True) # desired result
+            self.assertTrue(True)  # desired result
         else:
             self.assertNotEqual(fit_result.params, 'hello')
 
         try:
-            # Bypass the property getter. This will work, as it set's the instance value of __params.
+            # Bypass the property getter. This will work,
+            # as it set's the instance value of __params.
             fit_result.__params = 'hello'
-        except AttributeError as foo:
-            self.assertTrue(False) # undesired result
+        except AttributeError:
+            self.fail()
         else:
             self.assertNotEqual(fit_result.params, 'hello')
-            # The assginment will have succeeded on the instance because we set it from the outside.
-            # I must admit I don't fully understand why this is allowed and I don't like it.
-            # However, the tests below show that it did not influence the class method itself so
+            # The assginment will have succeeded on the instance because we
+            # set it from the outside. I must admit I don't fully understand
+            # why this is allowed and I don't like it. However, the tests
+            # below show that it did not influence the class method itself so
             # fitting still works fine.
             self.assertEqual(fit_result.__params, 'hello')
 
-        # Do a second fit and dubble check that we do not overwrtie something crusial.
+        # Do a second fit and double check that we do not overwrite
+        # something crucial.
         xdata = np.arange(-5, 5, 1)
         ydata = np.arange(-5, 5, 1)
         xx, yy = np.meshgrid(xdata, ydata, sparse=False)
@@ -94,7 +103,7 @@ class TddInPythonExample(unittest.TestCase):
         self.assertAlmostEqual(fit_result_2.params.b, 3.0)
 
     def test_fitting(self):
-        xdata = np.linspace(1,10,10)
+        xdata = np.linspace(1, 10, 10)
         ydata = 3*xdata**2
 
         a = Parameter(3.0)
@@ -131,15 +140,14 @@ class TddInPythonExample(unittest.TestCase):
         self.assertRaises(AttributeError, getattr, *[fit_result.params, 'a_stdev_'])
         self.assertRaises(AttributeError, getattr, *[fit_result.params, 'a__stdev'])
 
-    def test_numpy_functions(self):
-        xdata = np.linspace(1,10,10)
-        ydata = 45*np.log(xdata*2)
+    #def test_numpy_functions(self):
+        #xdata = np.linspace(1, 10, 10)
+        #ydata = 45 * np.log(xdata * 2)
 
-        a = Parameter()
-        b = Parameter(value=2.1, fixed=True)
-        x = Variable()
-        new = a*sympy.log(x*b)
-
+        #a = Parameter()
+        #b = Parameter(value=2.1, fixed=True)
+        #x = Variable()
+        #new = a*sympy.log(x*b)
 
     def test_grid_fitting(self):
         xdata = np.arange(-5, 5, 1)
@@ -172,7 +180,6 @@ class TddInPythonExample(unittest.TestCase):
         self.assertAlmostEqual(results.params.a, 2.5)
         self.assertAlmostEqual(results.params.b, 3.)
 
-
     def test_2D_fitting(self):
         xdata = np.random.randint(-10, 11, size=(2, 400))
         zdata = 2.5*xdata[0]**2 + 7.0*xdata[1]**2
@@ -187,7 +194,6 @@ class TddInPythonExample(unittest.TestCase):
 
         result = fit.scipy_func(fit.xdata, [2, 3])
 
-        import inspect
         args, varargs, keywords, defaults = inspect.getargspec(fit.scipy_func)
         self.assertEqual(args, ['x', 'p'])
 
@@ -195,7 +201,7 @@ class TddInPythonExample(unittest.TestCase):
         self.assertIsInstance(fit_result, FitResults)
 
     def test_gaussian_fitting(self):
-        xdata = 2*np.random.rand(10000) - 1 # random betwen [-1, 1]
+        xdata = 2*np.random.rand(10000) - 1  # random between [-1, 1]
         ydata = scipy.stats.norm.pdf(xdata, loc=0.0, scale=1.0)
 
         x0 = Parameter()
@@ -222,16 +228,16 @@ class TddInPythonExample(unittest.TestCase):
 
     def test_2_gaussian_2d_fitting(self):
         np.random.seed(4242)
-        mean = (0.3, 0.3) # x, y mean 0.6, 0.4
-        cov = [[0.01**2,0],[0,0.01**2]]
+        mean = (0.3, 0.3)  # x, y mean 0.6, 0.4
+        cov = [[0.01**2, 0], [0, 0.01**2]]
         data = np.random.multivariate_normal(mean, cov, 1000000)
-        mean = (0.7,0.7) # x, y mean 0.6, 0.4
-        cov = [[0.01**2,0],[0,0.01**2]]
+        mean = (0.7, 0.7)  # x, y mean 0.6, 0.4
+        cov = [[0.01**2, 0], [0, 0.01**2]]
         data_2 = np.random.multivariate_normal(mean, cov, 1000000)
         data = np.vstack((data, data_2))
 
         # Insert them as y,x here as np fucks up cartesian conventions.
-        ydata, xedges, yedges = np.histogram2d(data[:,1], data[:,0], bins=100, range=[[0.0, 1.0], [0.0, 1.0]])
+        ydata, xedges, yedges = np.histogram2d(data[:, 1], data[:, 0], bins=100, range=[[0.0, 1.0], [0.0, 1.0]])
         xcentres = (xedges[:-1] + xedges[1:]) / 2
         ycentres = (yedges[:-1] + yedges[1:]) / 2
 
@@ -260,8 +266,8 @@ class TddInPythonExample(unittest.TestCase):
         fit = Fit(model, xdata, ydata)
         fit_result = fit.execute()
 
-        img = model(x=xx, y=yy, **fit_result.params)
-        img_g_1 = g_1(x=xx, y=yy, **fit_result.params)
+        #img = model(x=xx, y=yy, **fit_result.params)
+        #img_g_1 = g_1(x=xx, y=yy, **fit_result.params)
 
         # Equal up to some precision. Not much obviously.
         self.assertAlmostEqual(fit_result.params.x0_1, 0.7, 2)
@@ -270,36 +276,35 @@ class TddInPythonExample(unittest.TestCase):
         self.assertAlmostEqual(fit_result.params.y0_2, 0.3, 2)
 
     def test_gaussian_2d_fitting(self):
-        mean = (0.6,0.4) # x, y mean 0.6, 0.4
-        cov = [[0.2**2,0],[0,0.1**2]]
+        mean = (0.6, 0.4)  # x, y mean 0.6, 0.4
+        cov = [[0.2**2, 0], [0, 0.1**2]]
         data = np.random.multivariate_normal(mean, cov, 1000000)
 
         # Insert them as y,x here as np fucks up cartesian conventions.
-        ydata, xedges, yedges = np.histogram2d(data[:,0], data[:,1], bins=100, range=[[0.0, 1.0], [0.0, 1.0]])
+        ydata, xedges, yedges = np.histogram2d(data[:, 0], data[:, 1], bins=100, range=[[0.0, 1.0], [0.0, 1.0]])
         xcentres = (xedges[:-1] + xedges[1:]) / 2
         ycentres = (yedges[:-1] + yedges[1:]) / 2
 
         # Make a valid grid to match ydata
         xx, yy = np.meshgrid(xcentres, ycentres, sparse=False)
-        xdata = np.dstack((xx, yy)).T # T because np fucks up conventions.
+        xdata = np.dstack((xx, yy)).T  # T because np fucks up conventions.
 
         x0 = Parameter(0.6)
         sig_x = Parameter(0.2, min=0.0)
         x = Variable()
         y0 = Parameter(0.4)
         sig_y = Parameter(0.1, min=0.0)
-        A = Parameter()
+        A = Parameter(800)
         y = Variable()
         g = A * Gaussian(x, x0, sig_x) * Gaussian(y, y0, sig_y)
 
         fit = Fit(g, xdata, ydata)
         fit_result = fit.execute()
-
         # Again, the order seems to be swapped for py3k
-        self.assertAlmostEqual(fit_result.params.x0, np.mean(data[:,0]), 3)
-        self.assertAlmostEqual(fit_result.params.y0, np.mean(data[:,1]), 3)
-        self.assertAlmostEqual(np.abs(fit_result.params.sig_x), np.std(data[:,0]), 3)
-        self.assertAlmostEqual(np.abs(fit_result.params.sig_y), np.std(data[:,1]), 3)
+        self.assertAlmostEqual(fit_result.params.x0, np.mean(data[:, 0]), 3)
+        self.assertAlmostEqual(fit_result.params.y0, np.mean(data[:, 1]), 3)
+        self.assertAlmostEqual(np.abs(fit_result.params.sig_x), np.std(data[:, 0]), 3)
+        self.assertAlmostEqual(np.abs(fit_result.params.sig_y), np.std(data[:, 1]), 3)
         self.assertGreaterEqual(fit_result.r_squared, 0.99)
 
     def test_minimize(self):
@@ -308,7 +313,7 @@ class TddInPythonExample(unittest.TestCase):
         model = 2*x*y + 2*x - x**2 - 2*y**2
         from sympy import Eq, Ge
         constraints = [
-            Ge(y - 1, 0),  #y - 1 >= 0,
+            Ge(y - 1, 0),  # y - 1 >= 0,
             Eq(x**3 - y, 0),  # x**3 - y == 0,
         ]
 
@@ -335,7 +340,7 @@ class TddInPythonExample(unittest.TestCase):
             """ Derivative of objective function """
             dfdx0 = sign*(-2*x[0] + 2*x[1] + 2)
             dfdx1 = sign*(2*x[0] - 4*x[1])
-            return np.array([ dfdx0, dfdx1 ])
+            return np.array([dfdx0, dfdx1])
 
         cons = (
             {'type': 'eq',
@@ -345,12 +350,11 @@ class TddInPythonExample(unittest.TestCase):
              'fun' : lambda x: np.array([x[1] - 1]),
              'jac' : lambda x: np.array([0.0, 1.0])})
 
-        from scipy.optimize import minimize
-        res = minimize(func, [-1.0,1.0], args=(-1.0,), jac=func_deriv,
-               method='SLSQP', options={'disp': True})
+        res = minimize(func, [-1.0, 1.0], args=(-1.0,), jac=func_deriv,
+                       method='SLSQP', options={'disp': True})
 
-        res = minimize(func, [-1.0,1.0], args=(-1.0,), jac=func_deriv,
-               constraints=cons, method='SLSQP', options={'disp': True})
+        res = minimize(func, [-1.0, 1.0], args=(-1.0,), jac=func_deriv,
+                       constraints=cons, method='SLSQP', options={'disp': True})
 
     def test_likelihood_fitting(self):
         """
@@ -368,7 +372,6 @@ class TddInPythonExample(unittest.TestCase):
         fit_result = fit.execute()
 
         self.assertAlmostEqual(fit_result.params.b, 5., 1)
-
 
     def test_parameter_add(self):
         a = Parameter(value=1.0, min=0.5, max=1.5)
@@ -450,11 +453,11 @@ class TddInPythonExample(unittest.TestCase):
         self.assertAlmostEqual(fit_result.params.g_stdev, np.sqrt(pcov[0, 0]))
 
         self.assertAlmostEqual(fit_result.params.g, 9.095, 3)
-        self.assertAlmostEqual(fit_result.params.g_stdev, 0.102, 3) # according to Mathematica
+        self.assertAlmostEqual(fit_result.params.g_stdev, 0.102, 3)  # according to Mathematica
 
     def test_error_advanced(self):
         """
-        Models an example from the mathematica docs and try's to replicate it:
+        Models an example from the mathematica docs and attempts to replicate it:
         http://reference.wolfram.com/language/howto/FitModelsWithMeasurementErrors.html
         """
         data = [
@@ -478,7 +481,7 @@ class TddInPythonExample(unittest.TestCase):
 
         fit = Fit(model, xy, z, absolute_sigma=False)
         fit_result = fit.execute()
-        print(fit_result)
+#        print(fit_result)
 
         # Same as Mathematica default behavior.
         self.assertAlmostEqual(fit_result.params.a, 2.9956, 4)
@@ -508,9 +511,9 @@ class TddInPythonExample(unittest.TestCase):
         self.assertAlmostEqual(fit_result.params.a, popt[0], 4)
         self.assertAlmostEqual(fit_result.params.b, popt[1], 4)
         self.assertAlmostEqual(fit_result.params.c, popt[2], 4)
-        self.assertAlmostEqual(fit_result.params.a_stdev, np.sqrt(pcov[0,0]), 4)
-        self.assertAlmostEqual(fit_result.params.b_stdev, np.sqrt(pcov[1,1]), 4)
-        self.assertAlmostEqual(fit_result.params.c_stdev, np.sqrt(pcov[2,2]), 4)
+        self.assertAlmostEqual(fit_result.params.a_stdev, np.sqrt(pcov[0, 0]), 4)
+        self.assertAlmostEqual(fit_result.params.b_stdev, np.sqrt(pcov[1, 1]), 4)
+        self.assertAlmostEqual(fit_result.params.c_stdev, np.sqrt(pcov[2, 2]), 4)
 
         # Same as Mathematica with MEASUREMENT ERROR
         self.assertAlmostEqual(fit_result.params.a, 2.68807, 4)
@@ -575,9 +578,9 @@ class TddInPythonExample(unittest.TestCase):
         mean_xy = np.mean(x * y) - np.mean(x)*np.mean(y)
         a = mean_xy/mean_squared_x
         b = y.mean() - a * x.mean()
-        self.assertAlmostEqual(a, 0.694915, 6) # values from Mathematica
+        self.assertAlmostEqual(a, 0.694915, 6)  # values from Mathematica
         self.assertAlmostEqual(b, 0.186441, 6)
-        print(a, b)
+#        print(a, b)
 
         S = np.sum((y - (a*x + b))**2)
         var_a_exact = S/(len(x) * (len(x) - 2) * mean_squared_x)
@@ -593,7 +596,7 @@ class TddInPythonExample(unittest.TestCase):
 
         popt, pcov = curve_fit(lambda z, c, d: c * z + d, x, y,
                                Dfun=lambda p, x, y, func: np.transpose([x, np.ones_like(x)]))
-                                # Dfun=lambda p, x, y, func: print(p, func, x, y))
+                               # Dfun=lambda p, x, y, func: print(p, func, x, y))
 
         # curve_fit
         self.assertAlmostEqual(a_exact, popt[0], 4)
@@ -605,13 +608,6 @@ class TddInPythonExample(unittest.TestCase):
         self.assertAlmostEqual(b_exact, fit_result.params.b, 4)
         self.assertAlmostEqual(var_a_exact**0.5, fit_result.params.a_stdev, 6)
         self.assertAlmostEqual(var_b_exact**0.5, fit_result.params.b_stdev, 6)
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
