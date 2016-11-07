ba6c772230922f3408bbe5c46c42e2caff631146
Martin Roelfs <tbuli@rn-145-97-187-73.eduroam.rug.nl>
2016-11-07 15:43:12 +0100

    Added ConstrainedNumericalLeastSquares to the API, and did some cleanup

--- symfit/api.py
+++ symfit/api.py
@@ -2,9 +2,13 @@
 import symfit.core.operators
 
 # Expose useful objects.
-from symfit.core.fit import Fit, FitResults, Maximize, Minimize, Likelihood, Model, NumericalLeastSquares, LinearLeastSquares, NonLinearLeastSquares, TaylorModel, ODEModel
+from symfit.core.fit import (
+    Fit, FitResults, Maximize, Minimize, Likelihood, Model,
+    NumericalLeastSquares, LinearLeastSquares, NonLinearLeastSquares,
+    TaylorModel, ODEModel, ConstrainedNumericalLeastSquares
+)
 from symfit.core.argument import Variable, Parameter
-from symfit.core.support import variables, parameters
+from symfit.core.support import variables, parameters, D
 
 # Expose the sympy API
 from sympy import *
