d9ef052595f16689784faedb515aa0af95437622
Martin Roelfs <tbuli@rn-145-97-186-254.eduroam.rug.nl>
2016-12-12 18:16:45 +0100

    nitpick ignore module refs for now, and updated the build directory in conf so it is not scanned.

--- docs/conf.py
+++ docs/conf.py
@@ -37,10 +37,15 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-                       'sympy': ('http://docs.sympy.org/latest', None),
-                       'python': ('https://docs.python.org/3', None),
-                       'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
-                      }
+    'sympy': ('http://docs.sympy.org/latest', None),
+    'python': ('https://docs.python.org/3', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
+}
+
+# nitpick_ignore = [
+#     ('py:mod', 'symfit'),
+#     ('py:mod', 'scipy')
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -79,7 +84,7 @@ release = '0.3.5'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['docs_build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
