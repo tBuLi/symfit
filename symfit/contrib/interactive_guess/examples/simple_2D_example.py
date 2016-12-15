6bf9787e9e12ba5d4170255c944476caefeb113a
Martin Roelfs <tbuli@rn-145-97-186-38.eduroam.rug.nl>
2016-12-15 14:30:18 +0100

    Fixed imports in simple_2D_example.py

--- symfit/contrib/interactive_guess/examples/simple_2D_example.py
+++ symfit/contrib/interactive_guess/examples/simple_2D_example.py
@@ -4,8 +4,8 @@ Created on Mon Dec  7 11:28:58 2015
 
 @author: peterkroon
 """
-from symfit import Variable, Parameter, exp, NumericalLeastSquares, Model
-from symfit.contrib.interactive_guess import interactive_guess
+from symfit import Variable, Parameter, exp, Fit, Model
+from symfit.contrib.interactive_guess import InteractiveGuess2D
 import numpy as np
 
 
@@ -23,12 +23,12 @@ model = Model({y: distr(x, k, x0)})
 x_data = np.linspace(0, 2.5, 50)
 y_data = model(x=x_data, k=1000, x0=1).y
 
-guess = interactive_guess.InteractiveGuess2D(model, x=x_data, y=y_data)
+guess = InteractiveGuess2D(model, x=x_data, y=y_data)
 
 print("Guessed values: ")
 for p in guess.model.params:
     print("{}: {}".format(p.name, p.value))
 
-fit = ConstrainedNumericalLeastSquares(model, x=x_data, y=y_data)
+fit = Fit(model, x=x_data, y=y_data)
 fit_result = fit.execute(maxfev=1000)
 print(fit_result)
