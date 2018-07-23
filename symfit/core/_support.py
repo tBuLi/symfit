9a5180b97b033a2d8bf1edae9366a4cddf0e6e2c
Martin Roelfs <u0114255@kuleuven.be>
2018-07-23 17:05:33 +0200

    Fixed some typos in the docstring

--- symfit/core/_support.py
+++ symfit/core/_support.py
@@ -3,10 +3,10 @@ from functools import partial
 class repeatable_partial(partial):
     """
     In python < 3.5, stacked partials on the same function do not add more args
-    and kwargs to the function being partialed, but rather simple partial the
+    and kwargs to the function being partialed, but rather partial the
     partial.
 
-    This is unlogical behavior, which ghas been corrected in py35. This objects
+    This is unlogical behavior, which has been corrected in py35. This objects
     rectifies this behavior in earlier python versions as well.
 
     This is essentially just a copy-paste of python 3.5's __new__ method.
@@ -16,7 +16,6 @@ class repeatable_partial(partial):
             raise TypeError("descriptor '__new__' of partial needs an argument")
         if len(args) < 2:
             raise TypeError("type 'partial' takes at least one argument")
-        # cls, func, *args = args
         cls = args[0]
         func = args[1]
         args = args[2:]
