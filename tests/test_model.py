9e9456b8f68d4c78113d80da9888373e50de9df9
Martin Roelfs <u0114255@kuleuven.be>
2018-11-28 16:40:30 +0100

    Fixed py27 compatibility

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,10 @@ from __future__ import division, print_function
 import unittest
 from collections import OrderedDict
 import pickle
-from itertools import zip_longest
+try:
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest
 
 import numpy as np
 
