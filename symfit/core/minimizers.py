71e4b771ea2620905cece864463de6d2d1e0bc3a
Peter Kroon <p.c.kroon@rug.nl>
2018-12-03 14:44:19 +0100

    Fix typo in docstring

--- symfit/core/minimizers.py
+++ symfit/core/minimizers.py
@@ -156,7 +156,7 @@ class GradientMinimizer(BaseMinimizer):
 
 class HessianMinimizer(GradientMinimizer):
     """
-    ABC for Minimizer's that support the use of a Hessian.
+    ABC for Minimizers that support the use of a Hessian.
     """
     @keywordonly(hessian=None)
     def __init__(self, *args, **kwargs):
