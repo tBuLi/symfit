Changed the normalization of LeastSquares, such that the inverse Hessian is immidiately the covariance matrix. Fixed #197.

symfit/core/fit.py
@@ -1131,15 +1131,11 @@ class HasCovarianceMatrix(TakesData):
         except np.linalg.LinAlgError:
             return None
 
-        if isinstance(objective, LogLikelihood):
-            # Loglikelihood is a special case that needs to be considered
-            # separately, see #138.
-            return hess_inv
-        elif isinstance(objective, LeastSquares):
+        if isinstance(objective, LeastSquares):
             # Calculate the covariance for a least squares method.
             # https://www8.cs.umu.se/kurser/5DA001/HT07/lectures/lsq-handouts.pdf
             # Residual sum of squares
-            rss = objective(**key2str(best_fit_params))
+            rss = 2 * objective(**key2str(best_fit_params))
             # Degrees of freedom
             raw_dof = np.sum([np.product(shape) for shape in self.data_shapes[1]])
             dof = raw_dof - len(self.model.params)
@@ -1148,12 +1144,11 @@ class HasCovarianceMatrix(TakesData):
                 s2 = 1
             else:
                 s2 = rss / dof
-            # Multiply by two because the source uses different normalization
-            # ToDo: divide by two in the objective instead
-            cov_mat = 2 * s2 * hess_inv
+            cov_mat = s2 * hess_inv
             return cov_mat
         else:
-            # We do not know how to handle with this situation.
+            # The inverse hessian is the covariance matrix for Loglikelihood and
+            # also for objectives in general.
             return hess_inv
 
     def covariance_matrix(self, best_fit_params):

symfit/core/objectives.py
@@ -231,7 +231,15 @@ class VectorLeastSquares(GradientObjective):
 
 class LeastSquares(HessianObjective):
     """
-    Objective representing the :math:`\chi^2` of a model.
+    Objective representing the least-squares deviation of a model, defined as
+    :math:`\frac{1}{2} \sum_{i} \sum{x_i} \frac{r_i(x_i)^2}{\sigma_i(x_i)^2}`,
+    where :math:`i` sums over all components of the model, :math:`x_i`
+    indicates all the data associated with the :math:`i`-th component, and
+    :math:`\sigma_i(x_i)` indicates the associated standard deviations.
+
+    The data for each component does not have to be the same, and it does not
+    have to have the same shape. The only thing that matters is that within each
+    component the shapes have to be compatible.
     """
     @keywordonly(flatten_components=True)
     def __call__(self, ordered_parameters=[], **parameters):
@@ -258,7 +266,7 @@ class LeastSquares(HessianObjective):
                     (dep_var_value - dep_data) ** 2 / sigma ** 2
                 )
         chi2 = np.sum(chi2) if flatten_components else chi2
-        return chi2
+        return chi2 / 2
 
     def eval_jacobian(self, ordered_parameters=[], **parameters):
         """
@@ -285,7 +293,7 @@ class LeastSquares(HessianObjective):
                 sigma = self.sigma_data[sigma_var]
                 pre_sum = jac_comp * ((y - f) / sigma**2)[np.newaxis, ...]
                 axes = tuple(range(1, len(pre_sum.shape)))
-                result -= 2 * np.sum(pre_sum, axis=axes, keepdims=False)
+                result -= np.sum(pre_sum, axis=axes, keepdims=False)
         return np.atleast_1d(np.squeeze(np.array(result)))
 
     def eval_hessian(self, ordered_parameters=[], **parameters):
@@ -321,7 +329,7 @@ class LeastSquares(HessianObjective):
                 p2 = p2 / sigma[np.newaxis, np.newaxis, ...]**2
                 # We sum away everything except the matrices in the axes 0 & 1.
                 axes = tuple(range(2, len(p2.shape)))
-                result += 2 * np.sum(p2 - p1, axis=axes, keepdims=False)
+                result += np.sum(p2 - p1, axis=axes, keepdims=False)
         return np.atleast_2d(np.squeeze(np.array(result)))
 
 

tests/test_objectives.py
@@ -81,7 +81,7 @@ class TestObjectives(unittest.TestCase):
             x: xdata, y: ydata, model.sigmas[y]: np.ones_like(xdata)
         })
         chi2_exact = Model(
-            {X2: FlattenSum(((a * x ** 2 + b * x) - y) ** 2, i)})
+            {X2: FlattenSum(0.5 * ((a * x ** 2 + b * x) - y) ** 2, i)})
 
         eval_exact = chi2_exact(x=xdata, y=ydata, a=2, b=3)
         jac_exact = chi2_exact.eval_jacobian(x=xdata, y=ydata, a=2, b=3)
@@ -118,12 +118,9 @@ class TestObjectives(unittest.TestCase):
         fit_num_result = fit.execute()
         self.assertEqual(fit_exact_result.value(a), fit_num_result.value(a))
         self.assertEqual(fit_exact_result.value(b), fit_num_result.value(b))
-        # For MinimizeModel objectives the inverse hessian is returned as the
-        # covariance matrix. This is correct up to a factor of sqrt(2) in the
-        # case of a chi2.
-        self.assertAlmostEqual(np.sqrt(2) * fit_exact_result.stdev(a),
+        self.assertAlmostEqual(fit_exact_result.stdev(a),
                                fit_num_result.stdev(a))
-        self.assertAlmostEqual(np.sqrt(2) * fit_exact_result.stdev(b),
+        self.assertAlmostEqual(fit_exact_result.stdev(b),
                                fit_num_result.stdev(b))
 
 