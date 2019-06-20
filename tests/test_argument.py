3f9c9b07a414f01886c6de2d4f8cc5eddf019f58
Antony <tony.kamp@web.de>
2019-06-20 13:52:37 +0200

    Convert test_argument

--- tests/test_argument.py
+++ tests/test_argument.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function
 import pickle
-import unittest
+import pytest
 import sys
 import sympy
 import warnings
@@ -23,17 +23,16 @@ else:
     import funcsigs as inspect_sig
 
 
-class TestArgument(unittest.TestCase):
-    def test_parameter_add(self):
+def test_parameter_add():
         """
         Makes sure the __add__ method of Parameters behaves as expected.
         """
         a = Parameter(value=1.0, min=0.5, max=1.5)
         b = Parameter(value=1.0, min=0.0)
         new = a + b
-        self.assertIsInstance(new, sympy.Add)
+        assert isinstance(new, sympy.Add)
 
-    def test_argument_unnamed(self):
+def test_argument_unnamed():
         """
         Make sure the generated parameter names follow the pattern
         """
@@ -43,54 +42,33 @@ class TestArgument(unittest.TestCase):
         x = Variable()
         y = Variable('y')
 
-        self.assertEqual(str(a), '{}_{}'.format(a._argument_name, a._argument_index))
-        self.assertEqual(str(a), 'par_{}'.format(a._argument_index))
-        self.assertNotEqual(str(b), '{}_{}'.format(b._argument_name, b._argument_index))
-        self.assertEqual(str(c), '{}_{}'.format(c._argument_name, c._argument_index))
-        self.assertEqual(c.value, 10)
-        self.assertEqual(b.value, 10)
-        self.assertEqual(str(x), 'var_{}'.format(x._argument_index))
-        self.assertEqual(str(y), 'y')
+        assert str(a) == '{}_{}'.format(a._argument_name, a._argument_index)
+        assert str(a) == 'par_{}'.format(a._argument_index)
+        assert str(b) != '{}_{}'.format(b._argument_name, b._argument_index)
+        assert str(c) == '{}_{}'.format(c._argument_name, c._argument_index)
+        assert c.value == 10
+        assert b.value == 10
+        assert str(x) == 'var_{}'.format(x._argument_index)
+        assert str(y) == 'y'
 
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             d = Parameter(10)
 
 
-    def test_argument_name(self):
-        """
-        Make sure that Parameters have a name attribute with the expected
-        value.
-        """
-        a = Parameter()
-        b = Parameter(name='b')
-        c = Parameter(name='d')
-        self.assertNotEqual(a.name, 'a')
-        self.assertEqual(b.name, 'b')
-        self.assertEqual(c.name, 'd')
-
-    def test_symbol_add(self):
-        """
-        Makes sure the __add__ method of symbols behaves as expected.
-        """
-        x, y = sympy.symbols('x y')
-        new = x + y
-        self.assertIsInstance(new, sympy.Add)
-
-    def test_pickle(self):
+def test_pickle():
         """
         Make sure attributes are preserved when pickling
         """
         A = Parameter('A', min=0., max=1e3, fixed=True)
         new_A = pickle.loads(pickle.dumps(A))
-        self.assertEqual((A.min, A.value, A.max, A.fixed, A.name),
-                         (new_A.min, new_A.value, new_A.max, new_A.fixed, new_A.name))
+        assert (A.min, A.value, A.max, A.fixed, A.name) == (new_A.min, new_A.value, new_A.max, new_A.fixed, new_A.name)
 
         A = Parameter(min=0., max=1e3, fixed=True)
         new_A = pickle.loads(pickle.dumps(A))
-        self.assertEqual((A.min, A.value, A.max, A.fixed, A.name),
-                         (new_A.min, new_A.value, new_A.max, new_A.fixed, new_A.name))
+        assert (A.min, A.value, A.max, A.fixed, A.name) == (new_A.min, new_A.value, new_A.max, new_A.fixed, new_A.name)
 
-    def test_slots(self):
+
+def test_slots():
         """
         Make sure Parameters and Variables don't have a __dict__
         """
@@ -101,23 +79,12 @@ class TestArgument(unittest.TestCase):
         try:
             P.min = 0
         except AttributeError:
-            self.fail()
+            assert False
 
-        with self.assertRaises(AttributeError):
+        with pytest.raises(AttributeError):
             P.foo = None
 
         V = Variable('V')
-        with self.assertRaises(AttributeError):
+        with pytest.raises(AttributeError):
             V.bar = None
 
-
-if __name__ == '__main__':
-    try:
-        unittest.main(warnings='ignore')
-        # Note that unittest will catch and handle exceptions raised by tests.
-        # So this line will *only* deal with exceptions raised by the line
-        # above.
-    except TypeError:
-        # In Py2, unittest.main doesn't take a warnings argument
-        warnings.simplefilter('ignore')
-        unittest.main()
