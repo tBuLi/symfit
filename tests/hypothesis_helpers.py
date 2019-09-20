f73102e1024d90f299f3184c38e5bd8733485492
Peter Kroon <peterkroon06+github@gmail.com>
2019-09-20 16:28:38 +0200

    Add polynomial models, finish simple answer=guess test

--- tests/hypothesis_helpers.py
+++ tests/hypothesis_helpers.py
@@ -1,9 +1,6 @@
-from functools import reduce
-import operator
-
 import numpy as np
 
-from symfit import Model, parameters, variables, Variable
+from symfit import Model, variables
 from symfit.core.support import key2str
 import sympy
 
@@ -12,7 +9,13 @@ from hypothesis.extra import numpy as npst
 from hypothesis import assume, note
 
 
-NUMBERS = st.floats(allow_nan=False, allow_infinity=False, min_value=-1e3, max_value=1e3)
+MAX_VAL = 1e1
+MIN_VAL = 1e-1
+FLOATS = st.floats(allow_nan=False, allow_infinity=False,
+                   min_value=MIN_VAL, max_value=MAX_VAL)
+# Some hoops to jump through to limit the floats to MAX_VAL > abs(f) > MIN_VAL,
+# but still allow f=0
+NUMBERS = st.one_of(FLOATS.map(lambda f: -f), st.just(0), FLOATS)
 
 
 def _is_sorted(lst, cmp=lambda x, y: x <= y):
@@ -25,16 +28,22 @@ def _is_number(obj):
 
 
 @st.composite
-def exprs(draw, symbols, unary_ops, operations, n_leaves, allow_number=False):
+def expressions(draw, variables, parameters, constants, unary_ops, operations,
+                n_leaves, allow_number=False):
     """
     A strategy to generate arbitrary :mod:`Sympy` expressions, based on
     strategies for generating `symbols` and `operations`.
 
     Parameters
     ----------
-    symbols: hypothesis.LazySearchStrategy[sympy.Symbol]
-        A strategy that generates sympy symbols. Should probably include
-        constants such as 1 and -1.
+    variables: hypothesis.LazySearchStrategy[symfit.Variable]
+        A strategy that generates Variables. These variables will be
+        used as independent variables.
+    parameters: hypothesis.LazySearchStrategy[symfit.Parameter]
+        A strategy that generates Parameters.
+    constants: hypothesis.LazySearchStrategy[number]
+        A strategy that generates numbers. Should probably contain at least 1
+        and -1.
     unary_ops: hypothesis.LazySearchStrategy[sympy.Operation]
         A strategy that generates sympy operations that take a single argument.
     operations: hypothesis.LazySearchStrategy[tuple[int, sympy.Operation, bool]]
@@ -50,17 +59,24 @@ def exprs(draw, symbols, unary_ops, operations, n_leaves, allow_number=False):
     -------
     sympy.Expression
     """
-    symbol_st = st.lists(symbols, min_size=n_leaves, max_size=n_leaves)
+    if not allow_number:
+        first = st.one_of(variables, parameters)
+        rest = st.lists(st.one_of(constants, variables, parameters),
+                        min_size=n_leaves - 1, max_size=n_leaves - 1)
+        symbol_st = st.tuples(first, rest).map(lambda t: [t[0], *t[1]])
+    else:
+        symbol_st = st.lists(st.one_of(constants, variables, parameters),
+                             min_size=n_leaves, max_size=n_leaves)
     # Assert we draw at least one not numerical symbol, unless we allow numbers
-    leaves = draw(symbol_st.filter(
-        lambda l: allow_number or any(not _is_number(val) for val in l))
-    )
+    leaves = draw(symbol_st)
 
     # TODO: allow chaining of unary ops
     unary_ops = st.one_of(st.none(), unary_ops)
-    first_ops = draw(st.lists(unary_ops, min_size=len(leaves), max_size=len(leaves)))
+    first_ops = draw(st.lists(unary_ops,
+                              min_size=len(leaves),
+                              max_size=len(leaves)))
     leaves = [op(leaf) if op else leaf for op, leaf in zip(first_ops, leaves)]
- 
+
     while len(leaves) != 1:
         nargs, op, comm = draw(operations)
         # Draw indices for the leaves that get combined by the operation.
@@ -83,53 +99,147 @@ def exprs(draw, symbols, unary_ops, operations, n_leaves, allow_number=False):
     return expr
 
 
-# TODO: ODEModels
-@st.composite
-def models(draw, dependent_vars, symbols, leaves=5):
+def expression_strategy(variables, parameters, n_leaves=5):
     """
-    A strategy for generating :class:`symfit.Model` s. 
+    Helper function that calls :func:`expressions` with sane
+    defaults.
 
     Parameters
     ----------
-    dependent_vars: hypothesis.LazySearchStrategy[list[symfit.Variable]]
-        A strategy that generates lists of Variables. These variables will be
-        used as dependent variables.
-    symbols: hypothesis.LazySearchStrategy[symfit.Parameter or symfit.Variable]
-        A strategy that generates either a parameter or independent variable.
-    steps: int
-        The number of operations per expression. Larger numbers result is more
-        complex models.
+    variables: hypothesis.LazySearchStrategy[symfit.Variable]
+        A strategy that generates Variables. These variables will be
+        used as independent variables.
+    parameters: hypothesis.LazySearchStrategy[symfit.Parameter]
+        A strategy that generates Parameters.
+    n_leaves: int
+        The number of symbols to initially generate. Larger numbers result in
+        more complex expressions.
 
     Returns
     -------
-    symfit.Model
+    hypothesis.LazySearchStrategy[sympy.Expr]
     """
-    constants = st.sampled_from([1, -1, 2, 3, sympy.Rational(1, 2), sympy.pi, sympy.E])
-    symbols = st.one_of(constants, symbols)
+    constants = st.sampled_from([1, -1, 2, 3,
+                                 sympy.Rational(1, 2), sympy.pi, sympy.E])
     ops = st.sampled_from([(2, sympy.Add, True),
                            (2, sympy.Mul, True),
                            # Can cause imaginary numbers (e.g. (-4)**1.2)
                            # (2, sympy.Pow, False),
                            # (2, sympy.log, False),
-                          ])
+                           ])
     unary_ops = st.sampled_from([sympy.sin,
                                  sympy.exp,
                                  lambda e: -e,
                                  lambda e: 1/e])
+    return expressions(variables=variables, parameters=parameters,
+                       constants=constants, unary_ops=unary_ops,
+                       operations=ops, n_leaves=n_leaves)
+
+
+@st.composite
+def polynomial_expressions(draw, variables, parameters, constants,
+                           max_degree=10, max_terms=10):
+    """
+    Generates polynomial expressions that are linear in `parameters` and
+    `constants`. The produced polynomials will have at most `max_terms` terms,
+    and the degree will be between 1 and `max_degree`.
+
+    Parameters
+    ----------
+    variables: hypothesis.LazySearchStrategy[symfit.Variable]
+        A strategy that generates Variables. These variables will be
+        used as independent variables.
+    parameters: hypothesis.LazySearchStrategy[symfit.Parameter]
+        A strategy that generates Parameters.
+    constants: hypothesis.LazySearchStrategy[number]
+        A strategy that generates constants.
+    max_degree: int
+        Maximum degree of the produced polynomial
+    max_terms: int
+        Maximum number of terms of the produced polynomial
+
+    Returns
+    -------
+    hypothesis.LazySearchStrategy[sympy.Expr]
+    """
+    indep_vars = draw(st.lists(variables, unique=True, min_size=1))
+    orders = st.dictionaries(
+        keys=st.tuples(*[st.integers(min_value=0, max_value=max_degree)]*len(indep_vars)).filter(lambda t: 1 <= sum(t) <= max_degree),
+        values=st.one_of(parameters, constants),
+        min_size=1,
+        max_size=10
+    )
+    orders = draw(orders)
+    expr = sympy.Poly(orders, *indep_vars).as_expr()
+    return expr
+
+
+def polynomial_strategy(variables, parameters, max_degree=5, max_terms=10):
+    """
+    Helper function that calls :func:`polynomial_expressions` with sane
+    defaults.
+
+    Parameters
+    ----------
+    variables: hypothesis.LazySearchStrategy[symfit.Variable]
+        A strategy that generates Variables. These variables will be
+        used as independent variables.
+    parameters: hypothesis.LazySearchStrategy[symfit.Parameter]
+        A strategy that generates Parameters.
+    max_degree: int
+        Maximum degree of the produced polynomial
+    max_terms: int
+        Maximum number of terms of the produced polynomial
+
+    Returns
+    -------
+    hypothesis.LazySearchStrategy[sympy.Expr]
+    """
+    return polynomial_expressions(variables, parameters, st.just(1),
+                                  max_degree=max_degree, max_terms=max_terms)
+
+
+# TODO: ODEModels
+@st.composite
+def models(draw, dependent_vars, independent_vars, parameters, expressions_st,
+           allow_interdependent=True):
+    """
+    A strategy for generating :class:`symfit.Model` s. 
+
+    Parameters
+    ----------
+    dependent_vars: hypothesis.LazySearchStrategy[symfit.Variable]
+        A strategy that generates Variables. These variables will be
+        used as dependent variables.
+    independent_vars: hypothesis.LazySearchStrategy[symfit.Variable]
+        A strategy that generates Variables. These variables will be
+        used as independent variables.
+    parameters: hypothesis.LazySearchStrategy[symfit.Parameter]
+        A strategy that generates Parameters.
+    expressions_st: callable[(hypothesis.LazySearchStrategy[symfit.Variable],
+                              hypothesis.LazySearchStrategy[symfit.Parameter]),
+                             hypothesis.LazySearchStrategy[sympy.Expr]]
+        A function that when given variables and parameters, produces a strategy
+        that produces expressions.
+
+    Returns
+    -------
+    symfit.Model
+    """
     model_dict = {}
-    dep_vars = draw(dependent_vars)
+    dep_vars = draw(st.lists(dependent_vars, unique=True, min_size=1))
     for idx, dependent_var in enumerate(dep_vars):
         # Dependent vars can depend on other dependent vars: {y1: k, y2: y1+1}.
-        # But this can cause cyclical dependency ({y1: y2, y2: y1}), so limit to
-        # idx < jdx. Note that this also prevents {y1: y1}.
+        # But this can cause cyclical dependency ({y1: y2, y2: y1}), so limit
+        # to idx < jdx. Note that this also prevents {y1: y1}.
         other_vars = [dep_var for jdx, dep_var in enumerate(dep_vars) if idx < jdx]
-        if other_vars:
+        if other_vars and allow_interdependent:
             # Can't sample_from and empty list
-            symbs = st.one_of(symbols, st.sampled_from(other_vars))
+            indep_variables = st.one_of(independent_vars, st.sampled_from(other_vars))
         else:
-            symbs = symbols
-        expressions = exprs(symbs, unary_ops, ops, leaves, False)
-        expr = draw(expressions)
+            indep_variables = independent_vars
+        expression_st = expressions_st(indep_variables, parameters)
+        expr = draw(expression_st)
         model_dict[dependent_var] = expr
     model = Model(model_dict)
     return model
@@ -163,7 +273,8 @@ def _same_shape_vars(model):
 
 
 @st.composite
-def independent_data_shapes(draw, model, min_value=1, max_value=10, min_dim=1, max_dim=3):
+def independent_data_shapes(draw, model, min_num_values=0, min_value=1,
+                            max_value=10, min_dim=1, max_dim=3):
     """
     Strategy that generates shapes for the independent variables of `model` such
     that variables that need to be broadcast compatible will be the same shape.
@@ -187,7 +298,11 @@ def independent_data_shapes(draw, model, min_value=1, max_value=10, min_dim=1, m
     """
     indep_vars = model.independent_vars
     shape_groups = map(tuple, _same_shape_vars(model))
-    shapes = npst.array_shapes(min_dim, max_dim, min_value, max_value)
+    # Insist on a minimal number of data points to prevent underspecified models
+    # This is probably overly strict, since it enforces the same minimal shape
+    # on *all* components/variables. Instead it would probably be better to
+    # do this per element of model.connectivity_mapping
+    shapes = npst.array_shapes(min_dim, max_dim, min_value, max_value).filter(lambda shape: np.prod(shape) >= min_num_values)
     grp_shape = draw(st.fixed_dictionaries({var_grp: shapes for var_grp in shape_groups}))
     indep_shape = {}
     for vars, shape in grp_shape.items():
@@ -197,31 +312,12 @@ def independent_data_shapes(draw, model, min_value=1, max_value=10, min_dim=1, m
         # different.
         for var in vars[:-1]:
             indep_shape[var] = shape
-        indep_shape[vars[-1]] = npst.broadcastable_shapes(shape, min_dim, max_dim, min_value, max_value)
+        # Although technically possible, it can cause a bunch of numerical
+        # issues, and it's meaning is debatable at best.
+        # indep_shape[vars[-1]] = npst.broadcastable_shapes(shape, min_dim, max_dim, min_value, max_value)
+        indep_shape[vars[-1]] = shape
     return indep_shape
 
-
-def numpy_arrays(shape, elements=NUMBERS, **kwargs):
-    """
-    Basic strategy for generating numpy arrays of the specified `shape`, with
-    the specified `elements`.
-
-    Parameters
-    ----------
-    shape: tuple[int, ...]
-        The shape the array should get.
-    elements: hypothesis.LazySearchStrategy
-        The strategy which should be used to generate elements of the array.
-    kwargs: dict[str]
-        Additional keyword arguments to be passed to
-        :func:`hypothesis.extra.numpy.arrays`
-
-    Returns
-    -------
-    hypothesis.LazySearchStrategy[np.array]
-    """
-    return npst.arrays(float, shape, elements=NUMBERS, **kwargs)
-    
 
 @st.composite
 def indep_values(draw, model):
@@ -242,11 +338,12 @@ def indep_values(draw, model):
     dep_vars = model.dependent_vars
     params = model.params
 
-    indep_shape = draw(independent_data_shapes(model))
+    min_num_values = len(params) + 1
+    indep_shape = draw(independent_data_shapes(model, min_num_values))
 
     indep_vals = {}
     for var, shape in indep_shape.items():
-        data = numpy_arrays(shape, unique=True).map(np.sort)
+        data = npst.arrays(float, shape, elements=NUMBERS, unique=True).map(np.sort)
         indep_vals[str(var)] = data
 
     data = draw(st.fixed_dictionaries(indep_vals))
@@ -272,6 +369,24 @@ def param_values(draw, model):
     data = draw(param_vals)
     for param in model.params:
         param.value = data[str(param)]
+        lower, upper = draw(
+            st.lists(
+                st.one_of(
+                    st.none(),
+                    st.floats(0, 2*abs(param.value), allow_infinity=False, allow_nan=False)
+                ),
+                min_size=2,
+                max_size=2
+            )
+        )
+        if lower is not None:
+            param.min = param.value - lower
+        else:
+            param.min = None
+        if upper is not None:
+            param.max = param.value + upper
+        else:
+            param.max = None
     return data
 
 
@@ -298,7 +413,12 @@ def dep_values(draw, model, indep_data, param_vals):
         sigmas.
     """
     dep_data = model(**indep_data, **param_vals)._asdict()
-    # You can't provide data for interdependent variables.
+    # for var, data in dep_data.items():
+    #     if var in model.dependent_vars:
+    #         assume(np.all(((np.abs(data) >= MIN_VAL) | (data == 0)) &
+    #                       (np.abs(data) <= MAX_VAL)
+    #                       ))
+    # You can't provide data for interdependent variables. #270
     dep_data = {
         var: data
         for var, data in dep_data.items()
@@ -308,8 +428,8 @@ def dep_values(draw, model, indep_data, param_vals):
     sigmas = {
         'sigma_{}'.format(str(var)): st.one_of(
             st.none(),
-            st.floats(allow_nan=False, allow_infinity=False, min_value=0, max_value=5),
-            numpy_arrays(shape)
+            st.floats(allow_nan=False, allow_infinity=False, min_value=1e-6, max_value=5),
+            npst.arrays(float, shape, elements=st.floats(allow_nan=False, allow_infinity=False, min_value=1e-6, max_value=5))
         ) for var, shape in shapes.items()
     }
     sigmas = draw(st.fixed_dictionaries(dict(**sigmas)))
@@ -317,22 +437,21 @@ def dep_values(draw, model, indep_data, param_vals):
 
 
 @st.composite
-def model_with_data(draw, dependent_vars, symbols, leaves=5):
+def model_with_data(draw, dependent_vars, independent_vars, parameters, expr_strategy, **kwargs):
     """
     Strategy that generates a model with associated data using the given
     dependent variables.
 
     Parameters
     ----------
-    dependent_vars: hypothesis.LazySearchStrategy[list[symfit.Variable]]
+    dependent_vars: hypothesis.LazySearchStrategy[symfit.Variable]
         Strategy to use to generate dependent variables for the model.
-    symbols: hypothesis.LazySearchStrategy[symfit.Variable or symfit.Parameter]
-        Strategy to use to generate independent variables and parameters for the
-        model
-    leaves: int
-        The number of operations to perform when generating the expressions for
-        every dependent component of the model. More steps means complexer
-        expressions.
+    independent_vars: hypothesis.LazySearchStrategy[symfit.Variable]
+        Strategy to use to generate independent variables for the model.
+    parameters: hypothesis.LazySearchStrategy[symfit.Parameter]
+        Strategy to use to generate independent variables for the model.
+    expr_strategy: hypothesis.LazySearchStrategy[sympy.Expr]
+        Strategy to use to generate expressions for the model.
 
     Returns
     -------
@@ -345,7 +464,7 @@ def model_with_data(draw, dependent_vars, symbols, leaves=5):
     dict[str, np.array]
         Data for the dependent variables, as well as sigmas.
     """
-    model = draw(models(dependent_vars, symbols, leaves=leaves))
+    model = draw(models(dependent_vars, independent_vars, parameters, expr_strategy, **kwargs))
     indep_data, param_data = draw(st.tuples(indep_values(model), 
                                             param_values(model)),
                                   label='independent data, parameters')
@@ -355,13 +474,16 @@ def model_with_data(draw, dependent_vars, symbols, leaves=5):
         assume(False)  # Some model + data that causes numerical issues
     for data in dep_data.values():
         # In addition, let's make sure all data is finite and real.
-        assume(data is None or (np.all(np.isfinite(data)) and np.all(np.isreal(data))))
+        assume(data is None or (np.all(np.isfinite(data) & np.isreal(data))))
     return model, param_data, indep_data, dep_data
 
 
 if __name__ == '__main__':
-    DEPENDENT_VARS = st.lists(st.sampled_from(variables('Y1, Y2')), unique=True, min_size=1)
-    SYMBOLS = st.sampled_from(parameters('a, b, c') + variables('x, y, z'))
-
+    DEPENDENT_VARS = st.sampled_from(variables('Y1, Y2'))
+    INDEPENDENT_VARS = st.sampled_from(variables('x, y, z'))
+    PARAMETERS = st.sampled_from(variables('a, b, c'))
     for _ in range(5):
-        print(list(map(str, model_with_data(DEPENDENT_VARS, SYMBOLS).example())))
+        print(polynomial_expressions(INDEPENDENT_VARS, PARAMETERS, st.just(1)).example())
+
+    # for _ in range(5):
+    #     print(list(map(str, model_with_data(DEPENDENT_VARS, INDEPENDENT_VARS, PARAMETERS).example())))
