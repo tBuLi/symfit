828fab8433da8fbfc85d0745f2c42267de4d9860
Jochem Smit <jhsmit@gmail.com>
2020-02-06 16:44:56 +0100

    Update support.py
    Removed min/max check because its already in ``Parameter``'s init

--- symfit/core/support.py
+++ symfit/core/support.py
@@ -228,14 +228,6 @@ def parameters(names, **kwargs):
         else:
             sequences[attr] = kwargs.pop(attr)
 
-    if 'min' in sequences and 'max' in sequences:
-        for min, max in zip(sequences['min'], sequences['max']):
-            if min is None or max is None:
-                pass
-            elif min > max:
-                raise ValueError('The value of `min` should be less than or'
-                                 ' equal to the value of `max`.')
-
     params = symbols(names, cls=Parameter, seq=True, **kwargs)
     for key, values in sequences.items():
         try:
