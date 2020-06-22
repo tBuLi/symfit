0065fb5144cc4ddcf768fd27ec8c4d8962205f15
DuckNrOne <tony.kamp@web.de>
2020-06-22 19:29:11 +0200

    Added docstrings, w and d to auto_variable

--- tests/auto_variables.py
+++ tests/auto_variables.py
@@ -1,17 +1,31 @@
+"""
+This module is used to simplify tests by adding global variables and parameters. 
+The idea is that you can use them everywhere without referencing:
+In test cases, fixtures, or parametrize.
+You can use the variables w, x, y, z, and the parameters a, b, c, d.
+"""
 import symfit as sf
 import pytest
 
-x, y, z = sf.variables('x, y, z')
-a, b, c = sf.parameters('a, b, c')
+w, x, y, z = sf.variables('w, x, y, z')
+a, b, c, d = sf.parameters('a, b, c, d')
 
 
 @pytest.fixture(autouse=True)
 def initVariales():
-    global x, y, z
-    x, y, z = sf.variables('x, y, z')
-    
+    """
+    As autouse fixture this method re-initializes/resets the global variables w, x, y, z
+    for every test, no matter if parametrized or not. 
+    """
+    global w, x, y, z
+    w, x, y, z = sf.variables('w, x, y, z')
+
 
 @pytest.fixture(autouse=True)
 def initParameters():
-    global a, b, c
-    a, b, c = sf.parameters('a, b, c')
+    """
+    As autouse fixture, this method re-initializes/resets the global parameters a, b, c, d
+    for every test, no matter if parametrized or not. 
+    """
+    global a, b, c, d
+    a, b, c, d = sf.parameters('a, b, c, d')
