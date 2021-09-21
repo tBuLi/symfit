5d5da25bfe815488664b624985c9dad0f6152bc2
Jochem Smit <jhsmit@gmail.com>
2021-09-21 15:35:11 +0200

    Updated the Gaussian example to recent symfit standards

--- examples/gaussian.py
+++ examples/gaussian.py
@@ -1,18 +1,21 @@
-from __future__ import print_function
-from symfit import Parameter, Variable, Fit, exp
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+
+from symfit import Parameter, Variable, Fit, GradientModel
+from symfit.distributions import Gaussian
 
 palette = sns.color_palette()
 
 
-x = Variable()
-A = Parameter()
+x = Variable('x')
+y = Variable('y')
+A = Parameter('A')
 sig = Parameter(name='sig', value=1.4, min=1.0, max=2.0)
 x0 = Parameter(name='x0', value=15.0, min=0.0)
-# Gaussian distrubution
-model = A*exp(-((x - x0)**2/(2 * sig**2)))
+
+# Gaussian distribution
+model = GradientModel({y: A*Gaussian(x, x0, sig)})
 
 # Sample 10000 points from a N(15.0, 1.5) distrubution
 np.random.seed(seed=123456789)
@@ -22,10 +25,8 @@ xdata = (bin_edges[1:] + bin_edges[:-1])/2
 
 fit = Fit(model, xdata, ydata)
 fit_result = fit.execute()
-print(fit_result)
-print(model)
 
-y = model(x=xdata, **fit_result.params)
+y, = model(x=xdata, **fit_result.params)
 sns.regplot(xdata, ydata, fit_reg=False)
 plt.plot(xdata, y, color=palette[2])
 plt.ylim(0, 400)
