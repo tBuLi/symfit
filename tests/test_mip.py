af962d1ee7880e0cf42be5edae2230861b320245
Martin Roelfs <martin.roelfs@kuleuven.be>
2023-09-25 12:25:58 +0200

    Added simple mip test that can be compared with a Fit.

--- tests/test_mip.py
+++ tests/test_mip.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from symfit import Parameter
+from symfit import Parameter, parameters, Eq, MIP, Fit
 
 
 def test_parameter_array_bounds():
@@ -10,3 +10,23 @@ def test_parameter_array_bounds():
     with pytest.raises(ValueError):
         x = Parameter('x', min=ub, max=lb)
     x = Parameter('x', min=lb, max=ub)
+
+
+def test_bilinear():
+    # Create variables
+    x, y, z = parameters('x, y, z', min=0)
+
+    objective = 1.0 * x
+    constraints = [
+        x + y + z <= 10,
+        x * y <= 2,
+        Eq(x * z + y * z, 1),
+    ]
+
+    mip = MIP(- objective, constraints=constraints)
+    mip_result = mip.execute()
+    print(mip_result)
+
+    fit = Fit(- objective, constraints=constraints)
+    fit_result = fit.execute()
+    print(fit_result)
